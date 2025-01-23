7fdd49265c98db58b459d1f362384686ca96513a
santhipala <santhipala@gmail.com>
2025-01-22 18:32:39 -0700

    commit for Adding a reporting tool

--- features/environment.py
+++ features/environment.py
@@ -7,17 +7,22 @@ from app.application import Application
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver import Edge
 from selenium.webdriver.chrome.options import Options
+from app.application import Application
+from support.logger import logger
 def browser_init(context,scenario_name):
     """
     :param context: Behave context
     """
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
 
     # driver_path = GeckoDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Firefox(service=service)
+
+    # Command to run tests with Allure & Behave:
+    # behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/target_search.feature
 
     ## HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
@@ -30,19 +35,19 @@ def browser_init(context,scenario_name):
 
     ## BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = 'santhipala_4GEnaz'
-    bs_key = 'JY9EzQeoBJhApNp7NFLt'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-         "os" : "OS X",
-        "osVersion" : "Big Sur",
-        'browserName': 'safari',
-        'sessionName': scenario_name,
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'santhipala_4GEnaz'
+    # bs_key = 'JY9EzQeoBJhApNp7NFLt'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #      "os" : "OS X",
+    #     "osVersion" : "Big Sur",
+    #     'browserName': 'safari',
+    #     'sessionName': scenario_name,
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -52,15 +57,18 @@ def browser_init(context,scenario_name):
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context, scenario.name)
+    logger.info(f'Started scenario:,{scenario.name}')
 
 
 def before_step(context, step):
     print('\nStarted step: ', step)
+    logger.info(f'Started step:,{step}')
 
 
 def after_step(context, step):
     if step.status == 'failed':
         print('\nStep failed: ', step)
+        logger.error(f'Step failed: {step}')
 
 
 def after_scenario(context, feature):
